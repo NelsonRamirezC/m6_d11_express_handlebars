2c053e1bdb5b16fc1dfedae4ab46745d741ab4e6
Nelson Ramirez <xhyskoax@gmail.com>
2023-06-23 22:27:50 -0400

    Se agrega formulario para crear productos

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const { create } = require("express-handlebars");
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
 //MIDDLEWARE
 //DEJAMOS PUBLICA LA CARPETA PUBLIC
-app.use("/public", express.static(__dirname +"/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+/*
+si el cliente manda un objeto de notación javascript
+dicho objeto quedará guardado en req.body
+*/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //crear instancia de hanblebars con configuración de carpeta de partials
 const hbs = create({
@@ -25,7 +33,9 @@ app.listen(3000, () =>
 //RUTAS DE VISTAS
 //RUTA PRINCIPAL
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render("home", {
+        homePage: true,
+    });
 });
 
 let productos = [
@@ -59,7 +69,27 @@ app.get("/productos", (req, res) => {
     res.render("productos", {
         productos,
         titulo: "Lista de productos.",
+        productsPage: true,
     });
+});
+
+app.get("/productos/add", (req, res) => {
+    res.render("productosAdd");
+});
+
+//ENDPOINTS
+app.post("/api/productos", (req, res) => {
+    let { nombre, precio } = req.body;
+    let nuevoProducto = {
+        id: uuid().slice(0, 6),
+        nombre,
+        precio,
+        img: "producto1.jpg",
+    };
+
+    productos.push(nuevoProducto);
+
+    res.status(201).send({ code: 201, message: "Producto creado con éxito." });
 });
 
 app.all("*", (req, res) => {
